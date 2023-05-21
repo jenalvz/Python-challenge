92866855ad3f5a708fef2545b47bafc80e2c9499
Jennifer Alvarez <alvarezjennifer472@gmail.com>
2023-05-20 19:08:46 -0500

    upload

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+import csv
+
+
+file_budget = "Resources/budget_data.csv"
+
+total_profit = 0
+total_month = 0
+pre_profit = 0
+change = 0
+total_change = 0
+month_change = 0
+greatest_increase = 0
+greatest_increase_month = ""
+greatest_decrease = 0
+greatest_decrease_month = ""
+
+with open(file_budget) as input_file:
+    content = csv.reader(input_file)
+    next(content)
+
+    for row in content:
+        # print(row)
+        # exit()
+
+        total_profit = total_profit + int(row[1])
+        total_month = total_month + 1
+        
+        current_profit = int(row[1])
+
+        if pre_profit != 0:
+            change = current_profit - pre_profit
+            total_change = total_change + change
+            month_change = month_change + 1
+
+        pre_profit = current_profit
+
+        if change > greatest_increase:
+            greatest_increase = change
+            greatest_increase_month = row[0]
+
+
+        if change < greatest_decrease:
+            greatest_decrease = change
+            greatest_decrease_month = row[0]
+
+    avg_change = total_change/month_change
+    print(avg_change)
+
+
+print(total_profit)
+print(total_month)
+
+
+
+
+output = f"""
+Financial Analysis
+----------------------------
+Total Months: {total_month}
+Total: ${total_profit}
+Average Change: ${avg_change:,.2f}
+Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})
+Greatest Decrease in Profits: {greatest_decrease_month} ($ {greatest_decrease})
+"""
+
+print(output)
+
+with open("Analysis/pybank_output.txt", "w") as txt_file:
+    txt_file.write(output)
